90fa47882a269409e115ce86c9bf1f37a12981a2
Nbilabd <149062032+Nbilabd@users.noreply.github.com>
2023-11-20 22:14:05 +0800

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import pandas as pd
-
-# Memuat data
-file_name = "day.csv"
-bike_data = pd.read_csv(file_name)
 
 # Judul dashboard
 st.title("Dashboard Analisis Data Peminjaman Sepeda")
